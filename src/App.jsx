51393fe83d4d697f49e4859763489a4b834b5513
sahul-yadav <yadavsahul220@gmail.com>
2026-02-06 21:21:06 +0530

    Setup React Router and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 
 
+import { Router , Route, Routes} from 'react-router'
 import './App.css'
 import Card from './components/Card'
+import Home from './pages/Home'
+import Layouts from './layouts'
+import Products from './pages/Products'
+import ShowProducts from './pages/ShowProducts'
+import Cart from './pages/Cart'
+import Login from './pages/login'
+import Signup from './pages/signup'
 function App() {
   
 
   return (
     <>
+     <Routes>
+     <Route element={<Layouts/>}>
+      <Route path='/' element={<Home/>}/>
+      <Route path={`/product`}  element={<Products/>}/>
+      <Route path={`/user/:id`}  element={<Cart/>}/>
+      <Route  path={`/product/:id`} element={<ShowProducts/>}/>
+     </Route>
+     <Route path='/login' element={<Login/>} />
+     <Route  path='/signup' element={<Signup/>}/>
+     </Routes>
      <Card/>
     </>
   )
